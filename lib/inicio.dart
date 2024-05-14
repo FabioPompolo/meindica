7fee89c7265806a682205a0e4c9abb916c19f98c
Fabio <f.nassu@gmail.com>
2024-05-14 17:51:28 -0300

    troca de tela pós login

--- lib/inicio.dart
+++ lib/inicio.dart
@@ -1,183 +1,78 @@
 import 'package:flutter/material.dart';
-import 'list_screen.dart';
+import 'package:meindica/chat.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class NearbyWorkerListItem {
+  final String name;
+  final String avatarUrl;
+  final double distance;
+  final int rating;
+  final String category;
+
+  NearbyWorkerListItem(
+      {required this.name,
+      required this.avatarUrl,
+      required this.distance,
+      required this.rating,
+      required this.category});
+
+  static where(bool Function(dynamic mockedList) param0) {}
+}
+
+final List<NearbyWorkerListItem> mockedList = [
+  NearbyWorkerListItem(
+    name: 'Arvin Dragen',
+    avatarUrl:
+        'https://i.pinimg.com/originals/ec/49/fa/ec49fa73a1c51e53c3eb437b96165a52.jpg',
+    distance: 1.0,
+    rating: 5,
+    category: 'pintor',
+  ),
+  NearbyWorkerListItem(
+    name: 'Warrek Archyr',
+    avatarUrl:
+        'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ22ZtrTy3CurIt0XDGGcnjgiP3Pi_O_5na9ChgbsfA5iGSRpwv',
+    distance: 1.0,
+    rating: 5,
+    category: 'eletricista',
+  ),
+  NearbyWorkerListItem(
+      name: 'Barrian Staedmon',
+      avatarUrl:
+          'https://pics.craiyon.com/2023-09-11/94f949721ac34f5394a8d1be63247fdc.webp',
+      distance: 2.0,
+      rating: 4,
+      category: 'mecânico')
+];
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade50,
       appBar: AppBar(
         backgroundColor: Colors.grey.shade400,
-        title: const Text('Pesquisar Serviço'),
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            onPressed: () {
-              showSearch(context: context, delegate: CustomSearchDelegate());
-            },
-            icon: const Icon(Icons.search),
-          )
-        ],
-        centerTitle: true,
+        title: const Text('Profissionais perto de você'),
       ),
-      body: Center(
-        child: Stack(
-          children: <Widget>[
-            const Text(
-              "Olá, o que você precisa?",
-              style: TextStyle(fontSize: 20),
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.black,
-                    minimumSize: const Size(125, 125)),
-                onPressed: () {},
-                child: const Text('Pintor', style: TextStyle(fontSize: 20)),
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.black,
-                    minimumSize: const Size(125, 125),
-                    maximumSize: const Size(125, 125)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ListScreen()),
-                  );
-                },
-                child:
-                    const Text('Eletricista', style: TextStyle(fontSize: 19)),
-              ),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.black,
-                  minimumSize: const Size(125, 125),
-                  maximumSize: const Size(125, 125),
-                ),
-                onPressed: () {},
-                child: const Text('Encanador', style: TextStyle(fontSize: 18)),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.black,
-                    minimumSize: const Size(125, 125)),
-                onPressed: () {},
-                child: const Text('Jardineiro', style: TextStyle(fontSize: 20)),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.black,
-                    minimumSize: const Size(125, 125)),
-                onPressed: () {},
-                child: const Text('Pedreiro', style: TextStyle(fontSize: 20)),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.black,
-                    minimumSize: const Size(125, 125)),
-                onPressed: () {},
-                child: const Text('Montador', style: TextStyle(fontSize: 20)),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate {
-  List<String> searchTerms = [
-    "Último Serviço Pesquisado",
-    "Penúltimo Serviço Pesquisado",
-  ];
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = '';
-        },
-        icon: const Icon(Icons.clear),
-      ),
-    ];
-  }
-
-  // second overwrite to pop out of search menu
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: const Icon(Icons.arrow_back),
-    );
-  }
-
-  // third overwrite to show query result
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  // last overwrite to show the
-  // querying process at the runtime
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
+      backgroundColor: Colors.grey.shade50,
+      body: ListView.builder(
+          itemBuilder: (context, index) {
+            final worker = mockedList[index];
+            return ListTile(
+              onLongPress: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ChatScreen()),
+                );
+              },
+              leading:
+                  CircleAvatar(foregroundImage: NetworkImage(worker.avatarUrl)),
+              title: Text(worker.name),
+              subtitle: Text("${worker.distance}km"),
+              trailing: Text("${worker.category} "),
+            );
+          },
+          itemCount: mockedList.length),
     );
   }
 }
